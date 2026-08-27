f926019dbea9a228cb2082a409501223cc1af578
rjporosh <poroshdeveloper@gmail.com>
2026-08-27 21:17:09 +0600

    feat: configure api documentation and application infrastructure

--- src/Library.Api/Program.cs
+++ src/Library.Api/Program.cs
@@ -1,27 +1,36 @@
-using Library.Api.Endpoints;
 using Library.Application.DependencyInjection;
 using Library.Infrastructure.DependencyInjection;
 using Library.Infrastructure.Persistence.Repositories.InMemory.Seed;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
-
+// Application & Infrastructure
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure();
+
+// API
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.MapScalarApiReference(options =>
+    {
+        options
+            .WithTitle("Library Management System API")
+            .WithTheme(ScalarTheme.Mars)
+            .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+    });
 }
 
-app.UseHttpsRedirection();
-
-app.MapBookEndpoints();
-
+// Seed in-memory data
 var seeder = app.Services.GetRequiredService<InMemoryDataSeeder>();
 seeder.Seed();
+
+app.UseHttpsRedirection();
 
 app.Run();
