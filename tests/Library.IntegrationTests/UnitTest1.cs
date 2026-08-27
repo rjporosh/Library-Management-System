2fe8c3b1b9db97d173fbe3e1a24c85ddd84389bc
rjporosh <poroshdeveloper@gmail.com>
2026-08-27 19:13:53 +0600

    feat project initiated

---
+++ tests/Library.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/Library.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
